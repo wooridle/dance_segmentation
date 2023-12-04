74a55342f6e24f178510228e196717ffbf560a04
Ubuntu <ubuntu@ip-172-26-4-154.ap-northeast-2.compute.internal>
2023-12-04 09:26:45 +0000

    pickle added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('video.html')
+    return render_template('./video.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
